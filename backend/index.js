2eb26f092f8b1e0f173b93f29dc9f16fbe56eeb9
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-20 20:26:22 +0600

    .env removed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -8,7 +10,7 @@ app.use(cookieParser());
 
 
 
-
-app.listen(5000, (req, res) => {
-    console.log(`Server is Running on Port:5000`);
+const PORT = process.env.PORT || 5001
+app.listen(PORT, (req, res) => {
+    console.log(`Server is Running on Port:${PORT}`);
 });
